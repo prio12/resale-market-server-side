043934815576cdd9902329a2769fc62d6e18969f
MRPrio <maksudurprio.mp@gmail.com>
2022-11-30 01:07:45 +0600

    created api to get users

--- index.js
+++ index.js
@@ -18,24 +18,6 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-// function verifyJWT(req,res,next){
-
-// const authHeader = req.headers.authorization;
-// if(!authHeader){
-//   return res.status(401).send('unauthorized access');
-// }
-
-// const token = authHeader.split(' ')[1];
-
-// jwt.verify(token, process.env.ACCESS_TOKEN, function(err, decoded){
-//   if(err){
-//     return res.status(403).send({message: 'forbidden access'})
-//   }
-//   req.decoded = decoded;
-//   next();
-// })
-
-// }
 
 
 async function run(){
@@ -77,6 +59,13 @@ async function run(){
       res.send(result)
     })
 
+    app.get('/users', async(req,res) =>{
+      const role = req.query.role;
+      const query = {role: role}
+      const users = await userCollection.find(query).toArray();
+      res.send(users)
+    })
+
     //booking collection
 
     app.post('/bookings',async(req,res) =>{
@@ -88,28 +77,10 @@ async function run(){
     app.get('/bookings', async(req,res) =>{
       const email = req.query.email;
       console.log(email)
-      // const decodedEmail = req.decoded.email;
-
-      // if(email !== decodedEmail){
-      //   return res.status(403).send({message: 'forbidden access'})
-      // }
       const query = {email: email};
       const bookings = await bookingCollection.find(query).toArray();
       res.send(bookings)
     })
-
-
-    // app.get('/jwt', async(req,res) =>{
-    //   const email = req.query.email;
-    //   const query = {email:email};
-    //   const user = await userCollection.findOne(query);
-    //   if(user){
-    //     const token = jwt.sign({email},process.env.ACCESS_TOKEN, {expiresIn:'1h'})
-    //     return res.send({accessToken: token});
-    //   }
-    //   console.log(user);
-    //   res.status(403).send({accessToken:""})
-    // })
 
     }
 
