5099b1f4daacb6ae2fe881a504100d640fbda6cb
MRPrio <maksudurprio.mp@gmail.com>
2022-11-30 07:27:20 +0600

    seller and buyer deleted

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 // const jwt = require('jsonwebtoken')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 require('dotenv').config();
@@ -54,11 +54,45 @@ async function run(){
         
     })
 
+
+  app.get('/collection', async(req,res) =>{
+    let query ={};
+    if(req.query.email){
+      query={
+        email: req.query.email
+      }
+    }
+    const cursor = await phoneCollection.find(query).toArray();
+    res.send(cursor)
+    
+  })
+
+  app.delete('/collection/delete/:id',async(req,res) =>{
+    const id = req.params.id;
+    console.log(id)
+    const query = { _id: ObjectId(id)};
+    const result = await phoneCollection.deleteOne(query);
+    res.send(result)
+  })
+
+  app.delete('/buyer/delete/:id',async(req,res) =>{
+    const id = req.params.id;
+    console.log(id)
+    const query = { _id: ObjectId(id)};
+    const result = await userCollection.deleteOne(query);
+    res.send(result)
+  })
+  
+  app.delete('/seller/delete/:id',async(req,res) =>{
+    const id = req.params.id;
+    console.log(id)
+    const query = { _id: ObjectId(id)};
+    const result = await userCollection.deleteOne(query);
+    res.send(result)
+  })
+
+
     //users collection
-
-
-
-
     app.post('/users', async(req,res) =>{
       const user = req.body;
       const result = userCollection.insertOne(user);
